baea447ec9fa11a08e5ff498ec3f77c2f2840eb1
Stijn Taelman <stijn.taelman@gmail.com>
2025-12-29 16:37:03 +0100

    cleanup

---
+++ src/RoconMqtt/Can/Models/CommandType.cs
@@ -0,0 +1,22 @@
+﻿namespace RoconMqtt.Can.Models;
+
+/// <summary>
+/// Type of CAN communication command
+/// </summary>
+public enum CommandType
+{
+    /// <summary>
+    /// GET command - request to read a parameter value from the controller
+    /// </summary>
+    Get,
+
+    /// <summary>
+    /// SET command - request to write a parameter value to the controller
+    /// </summary>
+    Set,
+
+    /// <summary>
+    /// ANSWER command - response from controller containing parameter value
+    /// </summary>
+    Answer
+}
